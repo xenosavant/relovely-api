2a9441dd7a099acb7d4c4ee855bbce11dccfec05
Domenick Packett <Domenick.Packett@gmail.com>
2020-05-18 21:08:53 -0400

    feat: fb:

--- src/controllers/facebook/facebook.controller.ts
+++ src/controllers/facebook/facebook.controller.ts
@@ -67,10 +67,12 @@ export class FacebookController {
       return { error: 'This facebook account is already linked with an existing user' };
     }
 
-    // const stripeId = await this.stripeService.createCustomer(fbuser.email);
+    const stripeId = await this.stripeService.createCustomer(fbuser.email as string);
 
     const user = await this.userRepository.create({
       username: fbuser.name?.replace(' ', ''),
+      stripeId: stripeId,
+      email: fbuser.email,
       type: 'member',
       signedInWithFacebook: true,
       facebookAuthToken: longLivedToken.access_token,
