575c26effb6d52a86be7fb651c5bf161dc3acab3
Domenick Packett <Domenick.Packett@gmail.com>
2020-06-11 16:24:27 -0400

    debug: insta

--- src/services/instagram/instagram.service.ts
+++ src/services/instagram/instagram.service.ts
@@ -6,7 +6,6 @@ import { LongLivedTokenData } from '../response/long-lived-token-data';
 import { REFUSED } from 'dns';
 import { TokenServiceBindings } from '../../keys/token-service.bindings';
 import { TokenService } from '@loopback/authentication';
-import { HttpErrors } from '@loopback/rest';
 const client = require('request-promise');
 
 @bind({ scope: BindingScope.CONTEXT })
@@ -88,20 +87,21 @@ export class InstagramService {
       // headers: { 'Accept': 'application/json' }
     }
     let result: boolean = false;
-    // try {
-    let response = await client(options);
-    throw new HttpErrors.Conflict(response.body);
-    if (response.statusCode === 200) {
-      const regex = /Page Not Found/g;
-      if (regex.test(response.body)) {
+    try {
+      let response = await client(options);
+      if (response.statusCode === 404) {
         result = false;
-      } else {
-        result = true;
+      } else if (response.statusCode === 200) {
+        const regex = new RegExp(`/${username}/`, 'g');
+        if (regex.test(response.body)) {
+          result = true;
+        } else {
+          result = false;
+        }
       }
+    } catch {
+      result = false;
     }
-    // } catch {
-    //   result = false;
-    // }
     return result;
   }
 }
