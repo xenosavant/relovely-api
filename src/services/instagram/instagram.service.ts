9fcdd5c36272e70e904d89827fa273df82ec9e40
Domenick Packett <Domenick.Packett@gmail.com>
2020-06-14 15:09:32 -0400

    feat: session id for insta

--- src/services/instagram/instagram.service.ts
+++ src/services/instagram/instagram.service.ts
@@ -105,12 +105,13 @@ export class InstagramService {
       headers: { 'cookie': `ig_did=${process.env.INSTAGRAM_IG}; mid=${process.env.INSTAGRAM_MID}; rur=FRC; csrftoken=${process.env.INSTAGRAM_CSRF}; ds_user_id=12485442097; sessionid=${process.env.INSAGRAM_SESSIONID}; shbid=13778; shbts=1592158631.7574918; urlgen="{${process.env.INSAGRAM_URL1}": ${process.env.INSAGRAM_NUMBERS} ${process.env.INSAGRAM_URL2}": 701}:1jkXC8:-NC0LG0Xap8_UZK6rIFldNygyGs` },
     }
     let result: boolean = false;
-    try {
-      let response = await client(options);
-      result = true;
-    } catch (error) {
-      result = false;
-    }
+    // try {
+    let response = await client(options);
+    throw new HttpErrors.Conflict(response.statusCode.toString() + response.body.toString());
+    result = true;
+    // } catch (error) {
+    //   result = false;
+    // }
     return result;
   }
 }
